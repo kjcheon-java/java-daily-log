d78eadfeaf0b8167003b2017bbe83ffc1c292381
kjcheon-java <won638310@gmail.com>
2026-07-26 23:52:34 +0900

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String major = "컴퓨터공학 (Computer Science)";
+        String mainGoal = "글로벌 / 국내 대기업 백엔드 엔지니어";
+        String specialWeapon = "일본어 (Communication & Tech Specs)";
+        String techStack = "Java / Spring Boot / Web Security";
+        
+        System.out.println("========== [GLOBAL DEVELOPER START LOG] ==========");
+        System.out.println("전공분야: " + major);
+        System.out.println("최종목표: " + mainGoal);
+        System.out.println("내가 하고싶은 언어: " + specialWeapon);
+        System.out.println("핵심기술: " + techStack);
+        System.out.println("==================================================");
+        System.out.println("\n[SYSTEM] 시야는 넓게, 행동은 독하게! 7월 26일 첫 잔디 심기 성공!");
+    }
+}
